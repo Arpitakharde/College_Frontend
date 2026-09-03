a91646265ac82361c2e5aee272f3c51a8ad7431f
Arpitakharde <arpitakharde30@gmail.com>
2026-09-03 13:04:21 -0800

    add examination.jsx page in academics folder

---
+++ src/pages/academics/examination.jsx
@@ -0,0 +1,21 @@
+const Examination = () => {
+  return (
+    <main className="min-h-screen bg-gray-50 py-16 px-6">
+
+      <div className="max-w-6xl mx-auto">
+
+        <h1 className="text-4xl font-bold text-[#0b2d4d]">
+           Examination page
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          Welcome to PIRENS IBMA Examination Page.
+        </p>
+
+      </div>
+
+    </main>
+  );
+};
+
+export default Examination;
