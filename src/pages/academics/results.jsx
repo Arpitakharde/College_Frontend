2a9d943cde9a912a882427a54e8c491636a36b39
Arpitakharde <arpitakharde30@gmail.com>
2026-09-03 12:16:57 -0800

    Add results.jsx page

---
+++ src/pages/academics/results.jsx
@@ -0,0 +1,21 @@
+const Result = () => {
+  return (
+    <main className="min-h-screen bg-gray-50 py-16 px-6">
+
+      <div className="max-w-6xl mx-auto">
+
+        <h1 className="text-4xl font-bold text-[#0b2d4d]">
+           Result page
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          Welcome to PIRENS IBMA Result Page.
+        </p>
+
+      </div>
+
+    </main>
+  );
+};
+
+export default Result;
