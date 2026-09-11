db02aa90ee3e427351777e2307e8154559d59ddf
Arpitakharde <arpitakharde30@gmail.com>
2026-09-10 18:22:46 -0800

    anchor tag added for MBA result

--- src/pages/academics/results.jsx
+++ src/pages/academics/results.jsx
@@ -20,6 +20,17 @@ const Results = () => {
             <h2 className="text-3xl font-bold text-[#0b2d4d]">
               Annual Result of MBA
             </h2>
+
+            <a
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-block mt-16 bg-[#092f52] text-white px-12 py-7 text-2xl"
+            >
+              Click here for MBA Result
+            </a>
+
+
           </div>
 
 
