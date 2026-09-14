aeedb5a372a9841d4f0ab95979b3a73c8004e9bd
Arpitakharde <arpitakharde30@gmail.com>
2026-09-13 21:23:26 -0800

    images added

--- src/pages/academics/e-learning.jsx
+++ src/pages/academics/e-learning.jsx
@@ -1,23 +1,98 @@
-import ShodhSindhuimage from "../../assets/e-ShodhSindhu.png";
-import abc from "../../assets/pathshala.png";
-import virtualLabs from "../../assets/eyantra.png";
-import ndli from "../../assets/ndli.png";
-import banner from "../../assets/fossee.png";
-import epgPathshala from "../../assets/hero.png";
-import virtualLabs from "../../assets/nptel.png";
-import ndli from "../../assets/ndli.png";
-import epgPathshala from "../../assets/swayam_prabha.png";
-import virtualLabs from "../../assets/swayam.png";
-import ndli from "../../assets/VIDWAN.png";
-import virtualLabs from "../../assets/virtual_lap.png";
-import ndli from "../../assets/VIDWAN.png";
+import shodhSindhu from "../../assets/e-ShodhSindhu.png";
 
+import pathshala from "../../assets/pathshala.png";
 
+import eYantra from "../../assets/eyantra.png";
+
+import ndli from "../../assets/ndli.png";
+
+import fossee from "../../assets/fossee.png";
+
+import nptel from "../../assets/nptel.png";
+
+import swayamPrabha from "../../assets/swayam_prabha.png";
+
+import swayam from "../../assets/swayam.png";
+
+import vidwan from "../../assets/VIDWAN.png";
+
+import virtualLab from "../../assets/virtual_lap.png";
 
 
 
 const Elearning = () => {
 
+  const resources = [
+    {
+      id: 1,
+      name: "e-ShodhSindhu",
+      image: shodhSindhu,
+      link: "https://ess.inflibnet.ac.in/",
+    },
+
+    {
+      id: 2,
+      name: "e-PG Pathshala",
+      image: pathshala,
+      link: "https://epgp.inflibnet.ac.in/",
+    },
+
+    {
+      id: 3,
+      name: "e-Yantra",
+      image: eYantra,
+      link: "https://www.e-yantra.org/",
+    },
+
+    {
+      id: 4,
+      name: "National Digital Library of India",
+      image: ndli,
+      link: "https://ndl.iitkgp.ac.in/",
+    },
+
+    {
+      id: 5,
+      name: "FOSSEE",
+      image: fossee,
+      link: "https://fossee.in/",
+    },
+
+    {
+      id: 6,
+      name: "NPTEL",
+      image: nptel,
+      link: "https://nptel.ac.in/",
+    },
+
+    {
+      id: 7,
+      name: "SWAYAM Prabha",
+      image: swayamPrabha,
+      link: "https://www.swayamprabha.gov.in/",
+    },
+
+    {
+      id: 8,
+      name: "SWAYAM",
+      image: swayam,
+      link: "https://swayam.gov.in/",
+    },
+
+    {
+      id: 9,
+      name: "VIDWAN",
+      image: vidwan,
+      link: "https://vidwan.inflibnet.ac.in/",
+    },
+
+    {
+      id: 10,
+      name: "Virtual Labs",
+      image: virtualLab,
+      link: "https://www.vlab.co.in/",
+    },
+  ];
 
   
   return (
