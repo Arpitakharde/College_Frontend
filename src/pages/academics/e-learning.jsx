162436d830b1cf436bf00bcb85b67cbedf7a09ed
Arpitakharde <arpitakharde30@gmail.com>
2026-09-13 20:05:06 -0800

    images are import from asset folder

--- src/pages/academics/e-learning.jsx
+++ src/pages/academics/e-learning.jsx
@@ -1,4 +1,25 @@
+import ShodhSindhuimage from "../../assets/e-ShodhSindhu.png";
+import abc from "../../assets/pathshala.png";
+import virtualLabs from "../../assets/eyantra.png";
+import ndli from "../../assets/ndli.png";
+import banner from "../../assets/fossee.png";
+import epgPathshala from "../../assets/hero.png";
+import virtualLabs from "../../assets/nptel.png";
+import ndli from "../../assets/ndli.png";
+import epgPathshala from "../../assets/swayam_prabha.png";
+import virtualLabs from "../../assets/swayam.png";
+import ndli from "../../assets/VIDWAN.png";
+import virtualLabs from "../../assets/virtual_lap.png";
+import ndli from "../../assets/VIDWAN.png";
+
+
+
+
+
 const Elearning = () => {
+
+
+  
   return (
     <main className="min-h-screen bg-gray-50 py-16 px-6">
 
